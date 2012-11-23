290a8d122d10c220768d609216951de09119be11
Samuel Lewis <samuelewis@gmail.com>
2012-11-23 14:29:48 -0500

    Copy pasta green screen

---
+++ ui_project/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ui_project
+{
+
+    using Microsoft.Kinect;
+    using System.IO;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private KinectSensor sensor;
+
+        private const DepthImageFormat DepthFormat = DepthImageFormat.Resolution320x240Fps30;
+
+        private const ColorImageFormat ColorFormat = ColorImageFormat.RgbResolution640x480Fps30;
+
+        private int depthWidth;
+
+        private int depthHeight;
+
+        private int colorToDepthDivisor;
+        private DepthImagePixel[] depthPixels;
+        private byte[] colorPixels;
+        private int[] greenScreenPixelData;
+        private ColorImagePoint[] colorCoordinates;
+        private WriteableBitmap colorBitmap;
+        private int opaquePixelValue = -1;
+        private WriteableBitmap playerOpacityMaskImage = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Enable(DepthFormat);
+
+                this.depthWidth = this.sensor.DepthStream.FrameWidth;
+
+                this.depthHeight = this.sensor.DepthStream.FrameHeight;
+
+                this.sensor.ColorStream.Enable(ColorFormat);
+
+                int colorWidth = this.sensor.ColorStream.FrameWidth;
+                int colorHeight = this.sensor.ColorStream.FrameHeight;
+
+                this.colorToDepthDivisor = colorWidth / this.depthWidth;
+
+                // Turn on to get player masks
+                this.sensor.SkeletonStream.Enable();
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthPixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                this.greenScreenPixelData = new int[this.sensor.DepthStream.FramePixelDataLength];
+
+                this.colorCoordinates = new ColorImagePoint[this.sensor.DepthStream.FramePixelDataLength];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(colorWidth, colorHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                this.Forgound.Source = this.colorBitmap;
+
+                // Add an event handler to be called whenever there is new depth frame data
+                this.sensor.AllFramesReady += this.SensorAllFramesReady;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for Kinect sensor's DepthFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            // in the middle of shutting down, so nothing to do
+            if (null == this.sensor)
+            {
+                return;
+            }
+
+            bool depthReceived = false;
+            bool colorReceived = false;
+
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (null != depthFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthPixels);
+
+                    depthReceived = true;
+                }
+            }
+
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (null != colorFrame)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+
+                    colorReceived = true;
+                }
+            }
+
+            // do our processing outside of the using block
+            // so that we return resources to the kinect as soon as possible
+            if (true == depthReceived)
+            {
+                this.sensor.CoordinateMapper.MapDepthFrameToColorFrame(
+                    DepthFormat,
+                    this.depthPixels,
+                    ColorFormat,
+                    this.colorCoordinates);
+
+                Array.Clear(this.greenScreenPixelData, 0, this.greenScreenPixelData.Length);
+
+                // loop over each row and column of the depth
+                for (int y = 0; y < this.depthHeight; ++y)
+                {
+                    for (int x = 0; x < this.depthWidth; ++x)
+                    {
+                        // calculate index into depth array
+                        int depthIndex = x + (y * this.depthWidth);
+
+                        DepthImagePixel depthPixel = this.depthPixels[depthIndex];
+
+                        int player = depthPixel.PlayerIndex;
+
+                        // if we're tracking a player for the current pixel, do green screen
+                        if (player > 0)
+                        {
+                            // retrieve the depth to color mapping for the current depth pixel
+                            ColorImagePoint colorImagePoint = this.colorCoordinates[depthIndex];
+
+                            // scale color coordinates to depth resolution
+                            int colorInDepthX = colorImagePoint.X / this.colorToDepthDivisor;
+                            int colorInDepthY = colorImagePoint.Y / this.colorToDepthDivisor;
+
+                            // make sure the depth pixel maps to a valid point in color space
+                            // check y > 0 and y < depthHeight to make sure we don't write outside of the array
+                            // check x > 0 instead of >= 0 since to fill gaps we set opaque current pixel plus the one to the left
+                            // because of how the sensor works it is more correct to do it this way than to set to the right
+                            if (colorInDepthX > 0 && colorInDepthX < this.depthWidth && colorInDepthY >= 0 && colorInDepthY < this.depthHeight)
+                            {
+                                // calculate index into the green screen pixel array
+                                int greenScreenIndex = colorInDepthX + (colorInDepthY * this.depthWidth);
+
+                                // set opaque
+                                this.greenScreenPixelData[greenScreenIndex] = opaquePixelValue;
+
+                                // compensate for depth/color not corresponding exactly by setting the pixel 
+                                // to the left to opaque as well
+                                this.greenScreenPixelData[greenScreenIndex - 1] = opaquePixelValue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // do our processing outside of the using block
+            // so that we return resources to the kinect as soon as possible
+            if (true == colorReceived)
+            {
+                // Write the pixel data into our bitmap
+                this.colorBitmap.WritePixels(
+                    new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                    this.colorPixels,
+                    this.colorBitmap.PixelWidth * sizeof(int),
+                    0);
+
+                if (this.playerOpacityMaskImage == null)
+                {
+                    this.playerOpacityMaskImage = new WriteableBitmap(
+                        this.depthWidth,
+                        this.depthHeight,
+                        96,
+                        96,
+                        PixelFormats.Bgra32,
+                        null);
+
+                    Forgound.OpacityMask = new ImageBrush { ImageSource = this.playerOpacityMaskImage };
+                }
+
+                this.playerOpacityMaskImage.WritePixels(
+                    new Int32Rect(0, 0, this.depthWidth, this.depthHeight),
+                    this.greenScreenPixelData,
+                    this.depthWidth * ((this.playerOpacityMaskImage.Format.BitsPerPixel + 7) / 8),
+                    0);
+            }
+        }
+    }
+}
